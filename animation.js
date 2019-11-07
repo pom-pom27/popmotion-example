79311a994ff23d08731de63985c7fe3d0cc95412
siotong <siotong@pop-os.localdomain>
2019-11-07 16:54:31 +0700

    initial commit

---
+++ animation.js
@@ -0,0 +1,24 @@
+const popmotion = require("popmotion");
+require("./useles");
+
+const { styler, spring, listen, pointer, value } = popmotion;
+
+const ball = document.querySelector(".box");
+const divStyler = styler(ball);
+const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+
+listen(ball, "mousedown touchstart").start(e => {
+  e.preventDefault();
+  pointer(ballXY.get()).start(ballXY);
+});
+
+listen(document, "mouseup touchend").start(() => {
+  spring({
+    from: ballXY.get(),
+    velocity: ballXY.getVelocity(),
+    to: { x: 0, y: 0 },
+    stiffness: 200,
+    mass: 8,
+    damping: 10
+  }).start(ballXY);
+});
